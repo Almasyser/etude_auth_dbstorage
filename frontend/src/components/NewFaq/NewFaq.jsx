130cb56f17db0a1d52f911116e6bf0218155d3e7
Almasyser <almasyser@wanadoo.fr>
2025-12-28 23:42:59 +0100

    GROS problemes de nav!!!

--- frontend/src/components/NewFaq/NewFaq.jsx
+++ frontend/src/components/NewFaq/NewFaq.jsx
@@ -7,7 +7,15 @@ function NewFaq({setShowNewFaq}) {
   const [texte, setTexte] = useState('');
   const { userToken } = useContext(AuthContext);
   const [message, setMessage] = useState(false);
-  const handleSubmit = (event) => {
+  function timeout(delay) {
+    return new Promise( res => setTimeout(res, delay) );
+  }
+  const handleClick = ()=>{
+    window.location.reload();
+    setShowNewFaq(false);
+  }
+  const handleSubmit = async(event) => {
+    await timeout(1000);
     event.preventDefault();
     const form = event.target;
     const formData = new FormData(form);
@@ -21,7 +29,6 @@ function NewFaq({setShowNewFaq}) {
       .then((response) => {
         if (response.status === 204) {
           setMessage(true);
-          window.location.reload();
         } else {
           setMessage(false);
         }
@@ -29,21 +36,26 @@ function NewFaq({setShowNewFaq}) {
       .catch((error) => {
         console.error(error.message);
       });
-    setShowNewFaq(false);
+      setShowNewFaq(true);
   }
+
   return (
-    <>
-      <form className="newfaq" onSubmit={handleSubmit}>Nouvelle question:
+    <div className="newfaq">
+      <div className="entete">
+        <h1>Nouvelle question:</h1>
+        <i className="fi fi-rr-users" onClick={handleClick}>&nbsp;Retour</i>
+      </div>
+      <form className="form-newfaq" onSubmit={handleSubmit}>
         <input type="text" name="question" className="sujet" onChange={(e)=> setSujet(e.target.value)} value={sujet} placeholder="Sujet"/>
         <input type="text" name="answer" className="texte" onChange={(e)=> setTexte(e.target.value)} value={texte} placeholder="Texte"/>
-        <button type="submit" className="btn">
+        {(sujet.length && texte.length)? <button type="submit" className="btn">
           Valider la question
-        </button>
+        </button> : null}
       </form>
       <div>
-        { message? <p>Nouvel suestion postée avec succès..</p>:<p>erreur à la publication</p>}
+        { message && <p className="msg-valide">Nouvel question postée avec succès..</p>}
       </div>
-    </>
+    </div>
   )
 }
 
